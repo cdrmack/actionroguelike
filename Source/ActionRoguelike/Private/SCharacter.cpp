567e8a39fcae5c46523eb60ccfec4515f30fb464
cdrmack <git.pynvt@passmail.net>
2024-10-15 09:07:58 +0200

    add enhanced input for WSAD and mouse look

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -2,6 +2,10 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 ASCharacter::ASCharacter()
 {
@@ -12,6 +16,10 @@ ASCharacter::ASCharacter()
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	SpringArmComp->bUsePawnControlRotation = true; // camera reacts to both: vertical and horizontal mouse movement
+	bUseControllerRotationYaw = false; // allows to "orbit" horizontally around the character (see face etc.)
+	GetCharacterMovement()->bOrientRotationToMovement = true; // rotate the character toward the direction of acceleration
 }
 
 void ASCharacter::BeginPlay()
@@ -27,5 +35,52 @@ void ASCharacter::Tick(float DeltaTime)
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	const APlayerController* PlayerController = GetController<APlayerController>();
+	const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+	check(Subsystem);
+
+	Subsystem->ClearAllMappings();
+	Subsystem->AddMappingContext(InputMappingContext, 0);
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	EnhancedInputComponent->BindAction(Input_Move, ETriggerEvent::Triggered, this, &ASCharacter::Move);
+	EnhancedInputComponent->BindAction(Input_LookMouse, ETriggerEvent::Triggered, this, &ASCharacter::LookMouse);
 }
 
+void ASCharacter::Move(const FInputActionInstance& Instance)
+{
+	// Coordinate system:
+	// X - forward
+	// Y - right
+	// Z - up
+	
+	FRotator ControlRotation = GetControlRotation();
+	ControlRotation.Pitch = 0.0f;
+	ControlRotation.Roll = 0.0f;
+
+	// Enhanced Input Mapping Context:
+	// W (forward): YXZ
+	// S (backward): YXZ + Negate
+	// A (left): Negate
+	// D (right)
+	
+	const FVector2D AxisValue = Instance.GetValue().Get<FVector2D>();
+	
+	// forward/backward
+	AddMovementInput(ControlRotation.Vector(), AxisValue.Y);
+
+	// left/right
+	const FVector RightVector = FRotationMatrix(ControlRotation).GetScaledAxis(EAxis::Y);
+	AddMovementInput(RightVector, AxisValue.X);
+}
+
+void ASCharacter::LookMouse(const FInputActionValue& InputValue)
+{
+	const FVector2D AxisValue = InputValue.Get<FVector2D>();
+
+	AddControllerYawInput(AxisValue.X);
+	AddControllerPitchInput(AxisValue.Y);
+}
--- Source/ActionRoguelike/Public/SCharacter.h
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
 class USpringArmComponent;
 class UCameraComponent;
+class UInputMappingContext;
 
 UCLASS()
 class ACTIONROGUELIKE_API ASCharacter : public ACharacter
@@ -25,6 +27,19 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UCameraComponent> CameraComp;
 
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> Input_Move;
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> Input_LookMouse;
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	void Move(const FInputActionInstance& Instance);
+	void LookMouse(const FInputActionValue& InputValue);
 };
