1f0a3efcfbf5a0d8f78c5b520ef5ad964d217d48
cdrmack <git.pynvt@passmail.net>
2024-10-17 08:36:16 +0200

    add explosive barrel

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,38 @@
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
+	StaticMeshComp->SetSimulatePhysics(true);
+	RootComponent = StaticMeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(StaticMeshComp);
+	RadialForceComp->SetAutoActivate(false); // otherwise it slowly keeps pushing things away
+	RadialForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+	RadialForceComp->Radius = 750.0f;
+	RadialForceComp->ImpulseStrength = 2500.0f;
+	RadialForceComp->bImpulseVelChange = true; // ignore mass of objects
+}
+
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StaticMeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+}
+
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+}
---
+++ Source/ActionRoguelike/Public/SExplosiveBarrel.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class UStaticMeshComponent;
+class URadialForceComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASExplosiveBarrel();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<URadialForceComponent> RadialForceComp;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit );
+};
