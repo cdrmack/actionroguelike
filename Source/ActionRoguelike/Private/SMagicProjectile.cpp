fb9cc23af7478dd99267842f8a727dbb948eaa39
cdrmack <git.pynvt@passmail.net>
2024-10-31 08:24:53 +0100

    ignore projectile's overlap when other actor is its instigator

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -18,7 +18,7 @@ void ASMagicProjectile::PostInitializeComponents()
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!OtherActor)
+	if (!OtherActor || OtherActor == GetInstigator())
 	{
 		return;
 	}
